34a5238dbfbd07900d63b579ddac861a631efe9c
Marvin Moldenhauer <39803750+Xenion1987@users.noreply.github.com>
2023-06-16 08:02:59 +0200

    Update post-create-command.sh (#2)

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,5 +1,38 @@
 #!/usr/bin/env bash
 
-python -m pip -qqq install --upgrade --user -r ./.devcontainer/requirements.txt
-pre-commit install
-pre-commit autoupdate
+function install_pip_requirements() {
+  pip3 -qq install --upgrade --user -r .devcontainer/requirements.txt
+}
+function pre_commit_autoupdate() {
+  pre-commit install
+  pre-commit autoupdate
+}
+function create_code_workspace() {
+  HEAD="{ \"folders\": ["
+  TAIL="],\"settings\": {} }"
+  WORKSPACE_LOCATIONS=(/home/vscode/workspace)
+
+  echo -e "${HEAD}" >"${HOME}/dev.code-workspace"
+
+  for WORKSPACE_LOCATION in "${WORKSPACE_LOCATIONS[@]}"; do
+    PROJECTS=$(ls -1 "${WORKSPACE_LOCATION}")
+    for PROJECT in ${PROJECTS}; do
+
+      MAIN="  { \"path\": \"${WORKSPACE_LOCATION}/${PROJECT}\" },"
+      echo -e "${MAIN}" >>"${HOME}/dev.code-workspace"
+
+    done
+  done
+
+  echo -e "${TAIL}" >>"${HOME}/dev.code-workspace"
+
+  echo "code -r ${HOME}/dev.code-workspace"
+}
+
+function main() {
+  install_pip_requirements
+  pre_commit_autoupdate
+  create_code_workspace
+}
+
+main "${@}"
